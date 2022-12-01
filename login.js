6106655d6221e41bf258e9b6bde8aa6c60d5614f
masaijyoti <ergsjyotimeena0@gmail.com>
2022-12-01 10:38:56 +0530

    task_completed

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
 let reg_mail="sayalip2316";
 let reg_password="12354";
+
+if(reg_mail='sayalip2316'){
+console.log('login successful');
+
+}else if(reg_password='12354'){
+    console.log('login successful);
+}
+else{
+console.log('please check your password');}
+
